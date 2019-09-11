acfce59cfd2b4a4acaf17562bbc1c59ca7f5b4ac
Andriy Shapoval <a_shap1@yahoo.ca>
2019-09-11 16:26:36 -0400

    In process

---
+++ studentdatabaseapplication/src/studentdatabaseapplication/Student.java
@@ -0,0 +1,47 @@
+package studentdatabaseapplication;
+
+import java.util.Scanner;
+
+public class Student {
+	private String firstName;
+	private String lastName;
+	private int gradeYear;
+	private String studentID;
+	private String courses;
+	private int tuitionBalance;
+	private static int costOfCourse = 600;
+	private static int id = 1000;
+	
+	// Constructor: prompts user to enter student's name and year
+	public Student() {
+		Scanner in = new Scanner(System.in);
+		System.out.print("Enter student first name: ");
+		this.firstName = in.nextLine();
+		
+		System.out.print("Enter student last name: ");
+		this.lastName = in.nextLine();
+		
+		System.out.println("1 - Freshman\2 - Sophmore\n3 - Junior\n4 - Senior\nEnter student class level: ");
+		this.gradeYear = in.nextInt();
+		
+		setStudentID();
+		System.out.println(firstName + " " + lastName + " " + gradeYear + " " + studentID);
+		
+		
+	}
+	// Generate an ID 
+	private void setStudentID() {
+		// Grade Level + ID
+		id++;
+		this.studentID = gradeYear + "" + id;
+	}
+	
+	// Enroll in courses
+	
+	// View balance
+	
+	// Pay tuition 
+	
+	// Show status
+
+}
---
+++ studentdatabaseapplication/src/studentdatabaseapplication/StudentDatabaseApp.java
@@ -0,0 +1,13 @@
+package studentdatabaseapplication;
+
+public class StudentDatabaseApp {
+
+	public static void main(String[] args) {
+		Student st1 =  new Student();
+		// Ask how many new user we want to add
+		
+		// Create n number of new students
+
+	}
+
+}
