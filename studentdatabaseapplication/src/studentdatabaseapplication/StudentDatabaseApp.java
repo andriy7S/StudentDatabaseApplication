2090f4d4bbf8a13844be39b6d481dcbf613fd7a9
Andriy Shapoval <a_shap1@yahoo.ca>
2019-09-12 12:19:30 -0400

    Here we are

--- studentdatabaseapplication/src/studentdatabaseapplication/StudentDatabaseApp.java
+++ studentdatabaseapplication/src/studentdatabaseapplication/StudentDatabaseApp.java
@@ -1,13 +1,24 @@
 package studentdatabaseapplication;
 
+import java.util.Scanner;
+
 public class StudentDatabaseApp {
 
 	public static void main(String[] args) {
-		Student st1 =  new Student();
+		
 		// Ask how many new user we want to add
+		System.out.println("Enter a number of new students to enroll: ");
+		Scanner in = new Scanner(System.in);
+		int numOfStudents = in.nextInt();
+		Student[] students = new Student[numOfStudents];
 		
 		// Create n number of new students
-
+        for (int i = 0; i < numOfStudents; i++) {
+        	students[i] =  new Student();
+        	students[i].enroll();
+        	students[i].payTuition();
+    		System.out.println(students[i].toString());
+        }
 	}
 
 }
